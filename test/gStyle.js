4d3ba1d06317e11e8233fed74f3f23c0867dffb7
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-20 22:12:39 +0100

    OpenWeather API Calls and Tool-tip Marker Updates.

--- test/gStyle.js
+++ test/gStyle.js
@@ -25,35 +25,38 @@ function initMap() {
         disableDefaultUI: true,
     }); // Create a new Map and center on Home
 
+    const weatherInfo = `<div class="weather" id="weather"></div>`;
+    const toolTipButtons = `<br><div class="modal__actions"><button class="btn btn--passive">Overview</button><button class="btn btn--success">Graphs</button></div>`;
+
     var markersArray = [
         {
             coords: home,
             iconImage: '/assets/images/150px-IRL_Dublin_flag.svg.png',
-            content: `<h3>Dublin, Ireland</h3><hr><p>6 Oldcourt Close, Ballycullen, D24 RHY2</p><p>Dublin, Ireland</p>`
+            content: `<h3>Dublin, Ireland</h3>${weatherInfo}<hr><p>6 Oldcourt Close, Ballycullen, D24 RHY2</p><p>Dublin, Ireland</p>${toolTipButtons}`
         },
         {
             coords: { lat: 51.5074, lng: 0.1278 },
-            content: `London, England: 51.5074° N, 0.1278° W`
+            content: `<p>London, England: 51.5074° N, 0.1278°</p>${weatherInfo} ${toolTipButtons}`
         },
         {
             coords: { lat: 54.5973, lng: -5.9301 },
-            content: `Belfast, Northern Ireland: 54.5973° N, 5.9301° W`
+            content: `<p>Belfast, Northern Ireland: 54.5973° N, 5.9301° W</p>${weatherInfo} ${toolTipButtons}`
         },
         {
             coords: { lat: 51.4816, lng: -3.1791 },
-            content: `Cardiff, Wales: 51.4816° N, 3.1791° W`
+            content: `<p>Cardiff, Wales: 51.4816° N, 3.1791° W</p>${weatherInfo} ${toolTipButtons}`
         },
         {
             coords: { lat: 55.9533, lng: -3.1883 },
-            content: `Edinburg, Scotland: 55.9533° N, 3.1883° W`
+            content: `<p>Edinburg, Scotland: 55.9533° N, 3.1883° W</p>${weatherInfo} ${toolTipButtons}`
         },
         {
             coords: { lat: 1.3521, lng: 103.8198 },
-            content: `Singapore, Singapore: 1.3521° N, 103.8198° E`
+            content: `<p>Singapore, Singapore: 1.3521° N, 103.8198° E</p>${weatherInfo} ${toolTipButtons}`
         },
         {
             coords: { lat: 59.3293, lng: 18.0686 },
-            content: `Stockholm, Sweden: 59.3293° N, 18.0686° E`
+            content: `<p>Stockholm, Sweden: 59.3293° N, 18.0686° E</p>${weatherInfo} ${toolTipButtons}`
         }
     ];
 
@@ -84,11 +87,45 @@ function initMap() {
 
             marker.addListener("mouseover", function () {
                 infoWindow.open(map, marker);
+                
+                let markerString = String(marker.position); // Convert marker.position Object to String to manipulate the lat and lon for the OpenWeather API call.
+                markerString = markerString.replace(/[()]/g, "");  // Remove parenthesis () from markerString: (lat, lon).
+                markerString = markerString.split(" ").join(""); // Remove whitespace in middle of markerString: lat, lon.
+                markerStringArray = markerString.split(","); // Split the String into an Array to provide the OpenWeather API call correctly formatted lat and lon.
+
+                /* console.log(markerString, markerStringArray); */
+
+                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${markerStringArray[0]}&lon=${markerStringArray[1]}&units=metric&appid=4788a47d724b35cf9cc4e281a1893b4c`)
+                    .then(response => response.json())
+                    .then(data => {
+                        let tempValue = parseInt(data['main']['temp']);
+                        let descValue = data['weather'][0]['description'];
+                        let nameValue = data['name'];
+
+                        /* console.log(tempValue, descValue, nameValue); */
+
+                        let weatherID = document.getElementById("weather").id = "weather" + nameValue;
+
+                        /* console.log(weatherID); */
+
+                        document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue;
+
+                        /* console.log(marker.content, marker.position); */
+                    })
             });
 
+            /* Added a timeout to give the user enough time to notice and click on the buttons */
             marker.addListener("mouseout", function () {
-                infoWindow.close(map, marker);
+                setTimeout(function () {
+                    infoWindow.close(map, marker);
+                }, 3000);
             });
+
+            /* Click ensures the tool-tip remains open, enabling us to click on the buttons before the tool-tip vanishes (mouseover, mouseout) */
+            /*
+                        marker.addListener("click", function () {
+                            infoWindow.open(map, marker);
+                        }); */
         }
     }
 
