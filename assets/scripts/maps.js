14be8636d1577b6ebe96fa8dad01375072b1f3d2
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-21 19:03:56 +0100

    Added Air Pressure from OpenWeather to Marker.

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -11,6 +11,11 @@ Added several Locations/Markers with iconImage, and Content ("tool-tips")
 Added Mouseover and Mouseout as it's user-friendly and performs nicer than Click
 Saving Click for JS Modal (Code Institute Milestone Project II)
 
+OpenWeather API - Shanjah Raj: https://www.youtube.com/watch?v=GXrDEA3SIOQ&t=472s and OpenWeather API Doc: https://openweathermap.org/api/one-call-api?gclid=Cj0KCQjws_r0BRCwARIsAMxfDRiC6VCy8j0Jlfc27LsuhT9RbEdMJu3T0d9Z12oRrBRMFemuwWGUKIMaAj5DEALw_wcB
+Using Lat Lon from marker.position Object converted to String, formatted, and converted to an Array used by the fetch-then-catch Promise.
+
+D3 API - 
+
 === HOW?
 HTML: Create the navigation elements with relevant classes and id's, to be referenced by CSS and modified via JS.
 CSS: Style the Google Maps navigation elements, .gm-style... classes.
@@ -89,8 +94,8 @@ function initMap() {
                 infoWindow.open(map, marker);
 
                 let markerString = String(marker.position); // Convert marker.position Object to String to manipulate the lat and lon for the OpenWeather API call.
-                markerString = markerString.replace(/[()]/g, "");  // Remove parenthesis () from markerString: (lat, lon).
-                markerString = markerString.split(" ").join(""); // Remove whitespace in middle of markerString: lat, lon.
+                markerString = markerString.replace(/[() ]/g, "");  // Remove whitespace and parenthesis () from markerString: (lat, lon).
+                /* markerString = markerString.split(" ").join(""); // Remove whitespace in middle of markerString: lat, lon. */
                 markerStringArray = markerString.split(","); // Split the String into an Array to provide the OpenWeather API call correctly formatted lat and lon.
 
                 /* console.log(markerString, markerStringArray); */
@@ -100,16 +105,17 @@ function initMap() {
                     .then(data => {
                         let tempValue = parseInt(data['main']['temp']);
                         let descValue = data['weather'][0]['description'];
+                        let airPressure = data['main']['pressure'];
                         let nameValue = data['name'];
 
                         /* console.log(tempValue, descValue, nameValue); */
                         if (document.getElementById("weather")) {
                             let weatherID = document.getElementById("weather").id = "weather" + nameValue;
-                            document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue;
+                            document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
                             console.log("If: ", weatherID);
                         } else {
                             let weatherID = "weather" + nameValue;
-                            document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue;
+                            document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
                             console.log("Else :", weatherID);
                         }
 
@@ -121,6 +127,7 @@ function initMap() {
 
                         /* console.log(marker.content, marker.position); */
                     })
+                    .catch(err => console.log(err));
             });
 
             /* Added a timeout to give the user enough time to notice and click on the buttons */
@@ -137,6 +144,49 @@ function initMap() {
                         }); */
         }
     }
+
+    /* JSON returned from OpenWeather API Call for { lat: 53.274346, lng: -6.348835 } (Firhouse, Dublin, Ireland)
+    https://api.openweathermap.org/data/2.5/weather?lat=53.274346&lon=-6.348835&units=metric&appid=4788a47d724b35cf9cc4e281a1893b4c
+
+        {
+            "coord": {
+                "lon": -6.35, "lat": 53.27
+            },
+            "weather": [{
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }],
+            "base": "stations",
+            "main": {
+                "temp": 13.61,
+                "feels_like": 9.16,
+                "temp_min": 12.78,
+                "temp_max": 14,
+                "pressure": 1020,
+                "humidity": 54
+            },
+            "visibility": 10000, "wind": {
+                "speed": 4.6,
+                "deg": 80
+            },
+            "clouds": {
+                "all": 13
+            }, "dt": 1587489390, "sys": {
+                "type": 1,
+                "id": 1568,
+                "country": "IE",
+                "sunrise": 1587445893,
+                "sunset": 1587497767
+            },
+            "timezone": 3600,
+            "id": 3315276,
+            "name": "Firhouse",
+            "cod": 200
+        }
+    */
+
 
     /* === WHY?
     To control the style of the Google Map navigation elements we replace the default controls.
