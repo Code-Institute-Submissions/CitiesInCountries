cb898d78a446b7252aa53c8ecac231d8efb86290
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-23 00:18:28 +0100

    Modals update dynamically. Cleanup.

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,25 +1,23 @@
-/** Single Map Marker and infoWindow */
-
 /* === WHY?
-To place map markers of places I've travelled to in both business and pleasure, giving the audience of what I've done, where, and why.
-Map Marker -- 'hover' --> Tool-tip -- 'click' --> Overview Modal -- 'click Detail Button' --> Detail Modal || 'click D3 Button' --> Graphs...
+    To place map markers of places I've travelled to in both business and pleasure, giving the audience of what I've done, where, and why.
+    Map Marker -- 'hover' --> Tool-tip -- 'click' --> Overview Modal -- 'click Detail Button' --> Detail Modal || 'click D3 Button' --> Graphs...
 
 
 === WHAT?
-Google Maps API - Bill Traversy @ Traversy Media: https://www.youtube.com/watch?v=Zxf1mnP5zcw and Code Institute Walkthrough by Matt Rudge.
-Added several Locations/Markers with iconImage, and Content ("tool-tips")
-Added Mouseover and Mouseout as it's user-friendly and performs nicer than Click
-Saving Click for JS Modal (Code Institute Milestone Project II)
+    Google Maps API - Bill Traversy @ Traversy Media: https://www.youtube.com/watch?v=Zxf1mnP5zcw and Code Institute Walkthrough by Matt Rudge.
+    Added several Locations/Markers with iconImage, and Content ("tool-tips")
+    Added Mouseover and Mouseout as it's user-friendly and performs nicer than Click
+    Saving Click for JS Modal (Code Institute Milestone Project II)
 
-OpenWeather API - Shanjah Raj: https://www.youtube.com/watch?v=GXrDEA3SIOQ&t=472s and OpenWeather API Doc: https://openweathermap.org/api/one-call-api?gclid=Cj0KCQjws_r0BRCwARIsAMxfDRiC6VCy8j0Jlfc27LsuhT9RbEdMJu3T0d9Z12oRrBRMFemuwWGUKIMaAj5DEALw_wcB
-Using Lat Lon from marker.position Object converted to String, formatted, and converted to an Array used by the fetch-then-catch Promise.
+    OpenWeather API - Shanjah Raj: https://www.youtube.com/watch?v=GXrDEA3SIOQ&t=472s and OpenWeather API Doc: https://openweathermap.org/api/one-call-api?gclid=Cj0KCQjws_r0BRCwARIsAMxfDRiC6VCy8j0Jlfc27LsuhT9RbEdMJu3T0d9Z12oRrBRMFemuwWGUKIMaAj5DEALw_wcB
+    Using Lat Lon from marker.position Object converted to String, formatted, and converted to an Array used by the fetch-then-catch Promise.
 
-D3 API - 
+    D3 API - 
 
 === HOW?
-HTML: Create the navigation elements with relevant classes and id's, to be referenced by CSS and modified via JS.
-CSS: Style the Google Maps navigation elements, .gm-style... classes.
-JS: Create and call custom functions, ZoomControl, MapType Control, and FullScreenControl.
+    HTML: Create the navigation elements with relevant classes and id's, to be referenced by CSS and modified via JS.
+    CSS: Style the Google Maps navigation elements, .gm-style... classes.
+    JS: Create and call custom functions, ZoomControl, MapType Control, and FullScreenControl.
 */
 
 function initMap() {
@@ -37,31 +35,52 @@ function initMap() {
         {
             coords: home,
             iconImage: '/assets/images/150px-IRL_Dublin_flag.svg.png',
-            content: `<h3>Dublin, Ireland</h3>${weatherInfo}<hr><p>6 Oldcourt Close, Ballycullen, D24 RHY2</p><p>Dublin, Ireland</p>${toolTipButtons}`
+            content: `<h3>Dublin, Ireland</h3>${weatherInfo}<hr><p>6 Oldcourt Close, Ballycullen, D24 RHY2</p><p>Dublin, Ireland</p>${toolTipButtons}`,
+            name: "Dublin, Ireland",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 51.5074, lng: 0.1278 },
-            content: `<p>London, England: 51.5074° N, 0.1278°</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>London, England: 51.5074° N, 0.1278°</p>${weatherInfo} ${toolTipButtons}`,
+            name: "London, England",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 54.5973, lng: -5.9301 },
-            content: `<p>Belfast, Northern Ireland: 54.5973° N, 5.9301° W</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>Belfast, Northern Ireland: 54.5973° N, 5.9301° W</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Belfast, Northern Ireland",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 51.4816, lng: -3.1791 },
-            content: `<p>Cardiff, Wales: 51.4816° N, 3.1791° W</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>Cardiff, Wales: 51.4816° N, 3.1791° W</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Cardiff, Wales",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 55.9533, lng: -3.1883 },
-            content: `<p>Edinburg, Scotland: 55.9533° N, 3.1883° W</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>Edinburg, Scotland: 55.9533° N, 3.1883° W</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Edinburg, Scotland",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 1.3521, lng: 103.8198 },
-            content: `<p>Singapore, Singapore: 1.3521° N, 103.8198° E</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>Singapore, Singapore: 1.3521° N, 103.8198° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Singapore, Singapore",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         },
         {
             coords: { lat: 59.3293, lng: 18.0686 },
-            content: `<p>Stockholm, Sweden: 59.3293° N, 18.0686° E</p>${weatherInfo} ${toolTipButtons}`
+            content: `<p>Stockholm, Sweden: 59.3293° N, 18.0686° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Stockholm, Sweden",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         }
     ];
 
@@ -74,7 +93,10 @@ function initMap() {
             position: props.coords,
             map: map,
             iconImage: props.iconImage,
-            content: props.content
+            content: props.content,
+            name: props.name,
+            overview: props.overview,
+            d3: props.d3
         });
 
         // Check for separate icon for this marker (see if iconImage exists)
@@ -103,10 +125,10 @@ function initMap() {
                 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${markerStringArray[0]}&lon=${markerStringArray[1]}&units=metric&appid=4788a47d724b35cf9cc4e281a1893b4c`)
                     .then(response => response.json())
                     .then(data => {
-                        let tempValue = parseInt(data['main']['temp']);
-                        let descValue = data['weather'][0]['description'];
-                        let airPressure = data['main']['pressure'];
-                        let nameValue = data['name'];
+                        let tempValue = parseInt(data["main"]["temp"]);
+                        let descValue = data["weather"][0]["description"];
+                        let airPressure = data["main"]["pressure"];
+                        let nameValue = data["name"];
 
                         /* console.log(tempValue, descValue, nameValue); */
                         if (document.getElementById("weather")) {
@@ -117,22 +139,12 @@ function initMap() {
                             statisticsButton(buttonIDStats);
 
                             document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
-                            console.log("If: ", weatherID, buttonIDOver, buttonIDStats);
                         } else {
                             let weatherID = "weather" + nameValue;
                             let buttonIDOver = "buttonOver" + nameValue;
                             let buttonIDStats = "buttonStats" + nameValue;
                             document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
-                            console.log("Else :", weatherID, buttonIDOver, buttonIDStats);
                         }
-
-                        /* let weatherID = document.getElementById("weather").id = "weather" + nameValue; */
-
-                        /* console.log(weatherID); */
-
-                        /* document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue; */
-
-                        /* console.log(marker.content, marker.position); */
                     })
                     .catch(err => console.log(err));
             });
@@ -143,14 +155,66 @@ function initMap() {
                     infoWindow.close(map, marker);
                 }, 3000);
             });
+        };
 
-            /* Click ensures the tool-tip remains open, enabling us to click on the buttons before the tool-tip vanishes (mouseover, mouseout) */
-            /*
-                        marker.addListener("click", function () {
-                            infoWindow.open(map, marker);
-                        }); */
+
+        /* === WHY?
+            To allow the client/end-user to click on buttons for further details to better understand the city/town (Overview), as well as relevant statistics about the place (Statistics).
+    
+        === WHAT?
+            D3 API - Graphs in Scalable Vector Graphics: https://github.com/d3/d3#installing
+    
+        === HOW?
+            HTML: Create the navigation elements with relevant classes and id's, to be referenced by CSS and modified via JS.
+            CSS: Style the Google Maps navigation elements, .gm-style... classes.
+            JS: Create and call custom functions, ZoomControl, MapType Control, and FullScreenControl.
+        */
+
+        const backdrop = document.getElementById("backdrop");
+
+        const toggleBackdrop = () => {
+            backdrop.classList.toggle("visible");
+        };
+
+        const addModal = document.getElementById("add-modal");
+
+        const toggleModal = () => {
+            addModal.classList.toggle("visible");
+        };
+
+        let overviewButton = (buttonIDOver, name) => {
+            let buttonOverview = document.getElementById(buttonIDOver);
+
+            const overviewModalHandler = () => {
+                toggleBackdrop();
+                toggleModal();
+                document.getElementById("modal-content").innerHTML = "Overview... " + marker.name;
+                console.log("Clicked on Overview Button", buttonIDOver);
+            }
+            buttonOverview.addEventListener("click", overviewModalHandler);
+        };
+
+        let statisticsButton = (buttonIDStats) => {
+            let buttonStats = document.getElementById(buttonIDStats);
+
+            const overviewModalHandler = () => {
+                toggleBackdrop();
+                toggleModal();
+                document.getElementById("modal-content").innerHTML = "Stats... " + marker.name;
+                console.log("Clicked on Statistics Button", buttonIDStats);
+            }
+            buttonStats.addEventListener("click", overviewModalHandler);
+        };
+
+        let closeButton = document.getElementById("close");
+
+        const closeButtonHandler = () => {
+            toggleModal();
+            toggleBackdrop();
         }
-    }
+
+        closeButton.addEventListener("click", closeButtonHandler);
+    };
 
     /* JSON returned from OpenWeather API Call for { lat: 53.274346, lng: -6.348835 } (Firhouse, Dublin, Ireland)
     https://api.openweathermap.org/data/2.5/weather?lat=53.274346&lon=-6.348835&units=metric&appid=4788a47d724b35cf9cc4e281a1893b4c
@@ -292,61 +356,3 @@ function exitFullscreen() {
         document.msCancelFullScreen();
     }
 }
-
-
-/* === WHY?
-    To allow the client/end-user to click on buttons for further details to better understand the city/town (Overview), as well as relevant statistics about the place (Statistics).
-
-    === WHAT?
-    D3 API - Graphs in Scalable Vector Graphics: https://github.com/d3/d3#installing
-
-    === HOW?
-    HTML: Create the navigation elements with relevant classes and id's, to be referenced by CSS and modified via JS.
-    CSS: Style the Google Maps navigation elements, .gm-style... classes.
-    JS: Create and call custom functions, ZoomControl, MapType Control, and FullScreenControl.
-*/
-
-const backdrop = document.getElementById("backdrop");
-
-const toggleBackdrop = () => {
-    backdrop.classList.toggle("visible");
-    console.log("Backdrop toggled");
-};
-
-const addModal = document.getElementById("add-modal");
-
-const toggleModal = () => {
-    addModal.classList.toggle("visible");
-    // toggleBackdrop();
-};
-
-let overviewButton = (buttonIDOver) => {
-    let buttonOverview = document.getElementById(buttonIDOver);
-
-    const overviewModalHandler = () => {
-        toggleBackdrop();
-        toggleModal();
-        console.log("Clicked on Overview Button", buttonIDOver);
-    }
-    buttonOverview.addEventListener("click", overviewModalHandler);
-};
-
-let statisticsButton = (buttonIDStats) => {
-    let buttonStats = document.getElementById(buttonIDStats);
-
-    const overviewModalHandler = () => {
-        toggleBackdrop();
-        toggleModal();
-        console.log("Clicked on Statistics Button", buttonIDStats);
-    }
-    buttonStats.addEventListener("click", overviewModalHandler);
-};
-
-let closeButton = document.getElementById("close");
-
-const closeButtonHandler = () => {
-    toggleModal();
-    toggleBackdrop();
-}
-
-closeButton.addEventListener("click", closeButtonHandler);
