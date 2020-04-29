ee57fd91ef9f95fd5a2a984d9ac6651a588736aa
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-29 23:24:23 +0100

    Test: added linear background effect to make it look appealing. Added CSS and JS to modify the Google Map Marker background. Google Maps forces styling via JS that can only be overriden by JS.

--- test/gStyle.js
+++ test/gStyle.js
@@ -54,7 +54,7 @@ function initMap() {
             iconImage: '/assets/images/150px-IRL_Dublin_flag.svg.png',
             content: `<h3>Dublin, Ireland</h3>${weatherInfo}<hr><p>6 Oldcourt Close, Ballycullen, D24 RHY2</p><p>Dublin, Ireland</p>${toolTipButtons}`,
             name: "Dublin, Ireland",
-            overview: `<div class="overview" id="overview"></div>`,
+            overview: `<div class="overview" id="overview"><a href="https://en.wikipedia.org/wiki/Dublin" target="_blank">Dublin city center is small and lively, and full of great pubs and restaurants, shopping districts, theaters, parks, government buildings, and museums.</a></div>`,
             d3: `<div class="d3" id="d3"></div>`
         },
         {
@@ -161,9 +161,275 @@ function initMap() {
             name: "Reykjavik, Iceland",
             overview: `<div class="overview" id="overview"></div>`,
             d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 48.8566, lng: 2.3522 },
+            content: `<p>Paris, France: 48.8566° N, 2.3522° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Paris, France",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 50.8503, lng: 4.3517 },
+            content: `<p>Brussels, Belgium: 50.8503° N, 4.3517° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Brussels, Belgium",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 40.4168, lng: -3.7038 },
+            content: `<p>Madrid, Spain: 40.4168° N, 3.7038° W</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Madrid, Spain",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 38.7223, lng: -9.1393 },
+            content: `<p>Lisbon, Portugal: 38.7223° N, 9.1393° W</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Lisbon, Portugal",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 41.9028, lng: 12.4964 },
+            content: `<p>Rome, Italy: 41.9028° N, 12.4964° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Rome, Italy",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 46.9480, lng: 7.4474 },
+            content: `<p>Bern, Switzerland: 46.9480° N, 7.4474° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Bern, Switzerland",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 47.1660, lng: 9.5554 },
+            content: `<p>Liechtenstein, Liechtenstein: 47.1660° N, 9.5554° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Liechtenstein, Liechtenstein",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 49.8153, lng: 6.1296 },
+            content: `<p>Luxembourg, Luxembourg: 49.8153° N, 6.1296° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Luxembourg, Luxembourg",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 43.9424, lng: 12.4578 },
+            content: `<p>San Marino, San Marino: 43.9424° N, 12.4578° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "San Marino, San Marino",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 42.5063, lng: 1.5218 },
+            content: `<p>Andorra la Vella, Andorra: 42.5063° N, 1.5218° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Andorra, Andorra",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 43.7384, lng: 7.4246 },
+            content: `<p>Monaco, Monaco: 43.7384° N, 7.4246° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Monaco, Monaco",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 41.9029, lng: 12.4534 },
+            content: `<p>Vatican City, Vatican City: 41.9029° N, 12.4534° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Vatican City, Vatican City",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 52.3667, lng: 4.8945 },
+            content: `<p>Amsterdam, Netherlands: 52.3667° N, 4.8945° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Amsterdam, Netherlands",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 52.5200, lng: 13.4050 },
+            content: `<p>Berlin, Germany: 52.5200° N, 13.4050° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Berlin, Germany",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 48.2082, lng: 16.3738 },
+            content: `<p>Vienna, Austria: 48.2082° N, 16.3738° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Vienna, Austria",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 46.0569, lng: 14.5058 },
+            content: `<p>Ljubljana, Slovenia: 46.0569° N, 14.5058° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Ljubljana, Slovenia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 45.8150, lng: 15.9819 },
+            content: `<p>Zagreb, Croatia: 45.8150° N, 15.9819° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Zagreb, Croatia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 43.8563, lng: 18.4131 },
+            content: `<p>Sarajevo, Bosnia and Herzegovina: 43.8563° N, 18.4131° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Sarajevo, Bosnia and Herzegovina",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 42.4304, lng: 19.2594 },
+            content: `<p>Podgorica, Montenegro: 42.4304° N, 19.2594° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Podgorica, Montenegro",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 41.9981, lng: 21.4254 },
+            content: `<p>Skopje, Republic of Macedonia: 41.9981° N, 21.4254° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Skopje, Republic of Macedonia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 41.3275, lng: 19.8187 },
+            content: `<p>Tirana, Albania: 41.3275° N, 19.8187° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Tirana, Albania",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 37.9838, lng: 23.7275 },
+            content: `<p>Athens, Greece: 37.9838° N, 23.7275° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Athens, Greece",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 42.6977, lng: 23.3219 },
+            content: `<p>Sofia, Bulgaria: 42.6977° N, 23.3219° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Sofia, Bulgaria",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 44.4268, lng: 26.1025 },
+            content: `<p>Bucharest, Romania: 44.4268° N, 26.1025° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Bucharest, Romania",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 39.9334, lng: 32.8597 },
+            content: `<p>Ankara, Turkey: 39.9334° N, 32.8597° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Ankara, Turkey",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 47.4979, lng: 19.0402 },
+            content: `<p>Budapest, Hungary: 47.4979° N, 19.0402° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Budapest, Hungary",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 48.1486, lng: 17.1077 },
+            content: `<p>Bratislava, Slovakia: 48.1486° N, 17.1077° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Bratislava, Slovakia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 47.0105, lng: 28.8638 },
+            content: `<p>Chisinau, Moldova: 47.0105° N, 28.8638° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Chisinau, Moldova",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 50.4501, lng: 30.5234 },
+            content: `<p>Kyiv, Ukraine: 50.4501° N, 30.5234° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Kyiv, Ukraine",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 55.7558, lng: 37.6173 },
+            content: `<p>Moscow, Russia: 55.7558° N, 37.6173° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Moscow, Russia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 35.8989, lng: 14.5146 },
+            content: `<p>Valletta, Malta: 35.8989° N, 14.5146° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Valletta, Malta",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 42.6629, lng: 21.1655 },
+            content: `<p>Prishtina,  Kosovo: 42.6629° N, 21.1655° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Prishtina,  Kosovo",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 51.1605, lng: 71.4704 },
+            content: `<p>Nur-Sultan, Kazakhstan: 51.1605° N, 71.4704° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Nur-Sultan, Kazakhstan",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 41.7151, lng: 44.8271 },
+            content: `<p>Tbilisi, Georgia: 41.7151° N, 44.8271° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Tbilisi, Georgia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 35.1856, lng: 33.3823 },
+            content: `<p>Nicosia, Cyprus: 35.1856° N, 33.3823° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Nicosia, Cyprus",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 53.9006, lng: 27.5590 },
+            content: `<p>Minsk, Belarus: 53.9006° N, 27.5590° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Minsk, Belarus",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 40.4093, lng: 49.8671 },
+            content: `<p>Baku, Azerbaijan: 40.4093° N, 49.8671° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Baku, Azerbaijan",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
+        },
+        {
+            coords: { lat: 40.1872, lng: 44.5152 },
+            content: `<p>Yerevan, Armenia: 40.1872° N, 44.5152° E</p>${weatherInfo} ${toolTipButtons}`,
+            name: "Yerevan, Armenia",
+            overview: `<div class="overview" id="overview"></div>`,
+            d3: `<div class="d3" id="d3"></div>`
         }
     ];
-
+    
     for (let i = 0; i < markersArray.length; i++) {
         addMarker(markersArray[i]);
     }
@@ -220,10 +486,15 @@ function initMap() {
                             configureButtonEventHandlers(buttonIDOver, buttonIDStats);
                             //overviewButton(buttonIDOver);
                             //statisticsButton(buttonIDStats);
+                        /* Together with CSS Styling this creates a linear background effect. */
+                            document.querySelector(".gm-style-iw-d").className = "";
+                            document.querySelector(".gm-style-iw-c").style = "padding: 12px";
 
                             document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
                         } else {
                             let weatherID = "weather" + nameValue;
+                            document.querySelector(".gm-style-iw-d").className = "";
+                            document.querySelector(".gm-style-iw-c").style = "padding: 12px";
                             document.getElementById(weatherID).innerHTML = tempValue + `° Celsius, ` + descValue + `, ` + airPressure + ` hPa`;
                             // Update weather data dynamically after each "Mouseover" on Map Marker.
                         }
@@ -232,11 +503,11 @@ function initMap() {
             });
 
             /* Added a timeout to give the user enough time to notice and click on the buttons */
-            marker.addListener("mouseout", () => {
+            /* marker.addListener("mouseout", () => {
                 setTimeout(() => {
                     infoWindow.close(map, marker);
                 }, 3000);
-            });
+            }); */
         };
 
 
