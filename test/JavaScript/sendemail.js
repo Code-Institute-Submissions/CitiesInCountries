4767c12c8157133941a578ab38f27178b955d623
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-30 16:48:52 +0100

    Test: updated CSS Footer Modal with status messages on success and failure. Added JS to sendemail.js with status message update and a timer of 5 seconds to clear the message, and also addded a form reset() to clear the input.

--- test/JavaScript/sendemail.js
+++ test/JavaScript/sendemail.js
@@ -5,9 +5,18 @@ function sendMail(contactForm) {
         "feedback": contactForm.feedback.value
     })
         .then(function (response) {
-            console.log('SUCCESS!', response.status, response.text);
+            // console.log('SUCCESS!', response.status, response.text);
+            document.querySelector("#sendmail-status").innerHTML = "Thank you! Email sent successfully.";
+            document.querySelector("#feedbackform").reset();
+            setTimeout(() => {
+                document.querySelector("#sendmail-status").innerHTML = "";
+            }, 5000);
         }, function (error) {
-            console.log('FAILED...', error);
+            // console.log('FAILED...', error);
+            document.querySelector("#sendmail-status").innerHTML = "Apologies, something went wrong. Please try again";
+            setTimeout(() => {
+                document.querySelector("#sendmail-status").innerHTML = "";
+            }, 5000);
         });
     return false;
 }
